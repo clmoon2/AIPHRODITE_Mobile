1213ccf74579c0e7525ad18ca2c9a42125c781fc
KedarPanchal <kedar.t.panchal@outlook.com>
2025-11-08 21:41:03 -0600

    Create router skeleton

--- app/(tabs)/(shop)/index.tsx
+++ app/(tabs)/(shop)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/(tabs)/(closet)/index.tsx to edit this screen.</Text>
+      <Text>Edit app/(tabs)/(shop)/index.tsx to edit this screen.</Text>
     </View>
   );
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,54 @@
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Tabs.Screen
+    <Tabs.Screen
         name="(home)"
         options={{
           title: "Home",
         }}
-      />
+    />
+
+    <Tabs.Screen
+        name="(feed)"
+        options={{
+          title: "Feed",
+        }}
+    />
+
+    
+    <Tabs.Screen
+        name="(post)"
+        options={{
+          title: "Post",
+        }}
+        listeners={{
+            tabPress: (e) => {
+                e.preventDefault();
+                console.log("Tab intercepted!");
+            }
+        }}
+    />
+
+    <Tabs.Screen
+        name="(shop)"
+        options={{
+          title: "Shop",
+        }}
+    />
+
+    <Tabs.Screen
+        name="(closet)"
+        options={{
+          title: "Closet",
+        }}
+    />
     </Tabs>
   );
 }

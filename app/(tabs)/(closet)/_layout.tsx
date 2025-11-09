553c0b9833d94a92ac9f42a270f5753d5b73feb8
KedarPanchal <kedar.t.panchal@outlook.com>
2025-11-08 20:29:30 -0600

    Create layout files and index files

---
+++ app/(tabs)/(closet)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function ClosetLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
